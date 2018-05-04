c62d7b79bdb68ebd8bb3d3ea56f9add66a1330d8
237628056@qq.com <237628056@qq.com>
2018-05-04 21:22:56 +0800

    add distributed

--- zhihuuser/settings.py
+++ zhihuuser/settings.py
@@ -69,6 +69,7 @@ DEFAULT_REQUEST_HEADERS = {
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'zhihuuser.pipelines.MongoPipeline': 300,
+    'scrapy_redis.pipelines.RedisPipeline': 301,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
@@ -93,3 +94,8 @@ ITEM_PIPELINES = {
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 MONGO_URI = 'localhost'
 MONGO_DATABASE = 'zhihu'
+
+SCHEDULER = "scrapy_redis.scheduler.Scheduler"
+DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
+
+REDIS_URL = 'redis://root:myredisserver@192.168.151.130:6379'
--- zhihuuser/spiders/zhihu.py
+++ zhihuuser/spiders/zhihu.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import json
-
 from scrapy import Spider, Request
 
 from zhihuuser.items import UserItem
